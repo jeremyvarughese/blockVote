be52da50f7e2c9a12d18a210187e876a6804ea09
Jeremy Varughese <jeremyvarughese3@gmail.com>
2024-03-21 11:25:58 +0530

    2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,11 @@ import { createConnection } from "typeorm";
 import app from "./server";
 import "dotenv/config";
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8001;
 
 createConnection()
   .then(async (connection) => {
     app.listen(port, () => console.log(`listening on port ${port} ... `));
   })
   .catch((error) => console.log(error));
+ 
